d6bfdac3e76913a1907f462bd1bdf20fe1ee6586
daohengjp009 <daoheng009@gmail.com>
2022-02-16 21:05:40 +0800

    dev.off()

--- plot1.R
+++ plot1.R
@@ -9,3 +9,4 @@ hcpfeb0102 <- hcp[hcp$Date == "2007-02-01" | hcp$Date == "2007-02-02",]
 hcpfeb0102$Global_active_power <- as.numeric(hcpfeb0102$Global_active_power)
 #plot 1
 plot_1 <- with(hcpfeb0102, hist(Global_active_power, main = "Global Active Power", xlab = "Global Active Power(killowatts)", col = "red"))
+dev.off()
